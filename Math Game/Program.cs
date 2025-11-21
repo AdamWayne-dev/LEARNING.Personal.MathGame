ddf0d2a2d807c08d16883f1f907ae93daf7165eb
Adam Wayne <adamwayne.dev@gmail.com>
2025-11-21 07:54:46 +0000

    Added some colour to the console app.

--- Math Game/Program.cs
+++ Math Game/Program.cs
@@ -9,10 +9,11 @@ gameHistory = new Dictionary<int, List<string>>();
 void GameMenu()
 {
     answersLog = new List<string>();
-
-    
-    Console.WriteLine("Welcome to the Math Game!");
-    Console.WriteLine("To Play the game, please press type '1' || To see your hi-scores, please type '2'.");
+    Console.WriteLine("-------------------------------------------------------------------------------------------------------");
+    RainbowText("~~~                              Welcome to the Math Game!                                          ~~~");
+    Console.WriteLine();
+    Console.WriteLine("~~~    To Play the game, please press type '1' || To see your hi-scores, please type '2'.           ~~~");
+    Console.WriteLine("-------------------------------------------------------------------------------------------------------");
     int playerOption = CheckAnswer();
     if (playerOption == 1) 
     {
@@ -21,11 +22,7 @@ void GameMenu()
     if (playerOption == 2)
     {
         LoadHiScores();
-        //Console.WriteLine("Hi-scores are not yet implemented. Please press enter to return to the main menu.");
-        //Console.ReadLine();
-        //GameMenu();
     }
-    
 }
 
 void LoadMathGame()
@@ -33,14 +30,20 @@ void LoadMathGame()
     currentQuestion = 0;
     questionMaxCount = 5;
     correctAnswers = 0; // reset the correct answer count.
-    Console.WriteLine("You will be asked a series of math questions.");
-    Console.WriteLine("Try to answer them correctly!");
-    Console.WriteLine("There are 4 difficulties, please type the corresponding number for each difficulty:");
-    Console.WriteLine("1 - Easy (Numbers 1-25) || 2 - Medium (Numbers 25-50) || 3 - Hard (Numbers 50-75) || 4 - Einstein (Numbers 75-100)");
+    Console.WriteLine("-------------------------------------------------------------------------------------------------------");
+    Console.WriteLine("~~~       You will be asked a series of math questions.                                             ~~~");
+    Console.WriteLine("~~~       Try to answer them correctly!                                                             ~~~");
+    Console.WriteLine("~~~       There are 4 difficulties, please type the corresponding number for each difficulty:       ~~~");
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine("~~~       1 - Easy  || 2 - Medium || 3 - Hard  || 4 - Einstein                                      ~~~");
+    Console.ResetColor();
+    Console.WriteLine("-------------------------------------------------------------------------------------------------------");
     difficulty = CheckAnswer();
     if (difficulty < 1 || difficulty > 4)
     {
-        Console.WriteLine("Invalid difficulty selected. Defaulting to difficulty 1.");
+        Console.WriteLine("-------------------------------------------------------------------------------------------------------");
+        Console.WriteLine("~~~              Invalid difficulty selected. Defaulting to difficulty 1.                           ~~~");
+        Console.WriteLine("-------------------------------------------------------------------------------------------------------");
         difficulty = 1;
     }
     GameLoop();
@@ -52,16 +55,20 @@ void LoadHiScores()
     {
         int key = entry.Key;
         List<string> value = entry.Value;
-
+        RainbowText("#######################################################################################################");
+        RainbowText("-------------------------------------------------------------------------------------------------------");
         Console.WriteLine($"\nGame #{key}:");
-
+        RainbowText("-------------------------------------------------------------------------------------------------------");
         foreach (string question in value)
         {
             Console.WriteLine($" - {question}");
         }
+        RainbowText("#######################################################################################################");
         Console.WriteLine("");
     }
-    Console.WriteLine("Please press '1' to go back to the Menu.");
+    Console.WriteLine("-------------------------------------------------------------------------------------------------------");
+    Console.WriteLine("~~~                          Please press '1' to go back to the Menu.                               ~~~");
+    Console.WriteLine("-------------------------------------------------------------------------------------------------------");
     int playerResponse = 0;
     while (playerResponse != 1)
     {
@@ -70,7 +77,9 @@ void LoadHiScores()
         {
             GameMenu();
         }
-        Console.WriteLine("Incorrect input, please try again!");
+        Console.WriteLine("---------------------------------------------------------------------------------------------------");
+        Console.WriteLine("~~~                       Incorrect input, please try again!                                    ~~~");
+        Console.WriteLine("---------------------------------------------------------------------------------------------------");
     }
 }
 void AskQuestion(int level)
@@ -81,8 +90,9 @@ void AskQuestion(int level)
         gameHistoryKeyCounter++;
         questionMaxCount *= level;
     }
-    
-    Console.WriteLine($"Question {currentQuestion} || Difficulty {difficulty}:");
+    Console.WriteLine("-------------------------------------------------------------------------------------------------------");
+    Console.WriteLine($"                                   Question {currentQuestion} || Difficulty {difficulty}:                           ");
+    Console.WriteLine("-------------------------------------------------------------------------------------------------------");
     Random rand = new Random();
 
     int num1 = rand.Next(1, 25 * level);
@@ -96,22 +106,28 @@ void AskQuestion(int level)
     switch (questionType)
     {
         case 1:
-            
-            Console.WriteLine($"What is {num1} + {num2}?");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"                                       What is {num1} + {num2}?                                   ");
+            Console.ResetColor();
             answer = CheckAnswer();
+
             question = $"What is {num1} + {num2}?\n Answer: {answer}";
             expectedAnswer = num1 + num2;
             GiveFeedback(answer, expectedAnswer);
             break;
         case 2:
-            Console.WriteLine($"What is {num1} - {num2}?");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"                                       What is {num1} - {num2}?                                   ");
+            Console.ResetColor();
             answer = CheckAnswer();
             question = $"What is {num1} - {num2}?\n Answer: {answer}";
             expectedAnswer = num1 - num2;
             GiveFeedback(answer, expectedAnswer);
             break;
         case 3:
-            Console.WriteLine($"What is {num1} * {num2}?");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"                                       What is {num1} * {num2}?                                   ");
+            Console.ResetColor();
             answer = CheckAnswer();
             question = $"What is {num1} * {num2}?\n Answer: {answer}";
             expectedAnswer = num1 * num2;
@@ -123,7 +139,9 @@ void AskQuestion(int level)
                 CheckDividendIsFullyDivisible(num1, out newNum);
                 num2 = newNum;
             }
-            Console.WriteLine($"What is {num1} / {num2}");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"                                       What is {num1} / {num2}                                    ");
+            Console.ResetColor();
             answer = CheckAnswer();
             question = $"What is {num1} / {num2}?\n Answer: {answer}";
             expectedAnswer = num1 / num2;
@@ -147,13 +165,17 @@ void GiveFeedback(int playerAnswer, int questionAnswer)
 {
     if (playerAnswer == questionAnswer)
     {
-        Console.WriteLine($"Correct! Well done. Please press enter to continue...");
+        Console.WriteLine("-------------------------------------------------------------------------------------------------------");
+        Console.WriteLine($"~~~                   Correct! Well done. Please press enter to continue...                        ~~~");
+        Console.WriteLine("-------------------------------------------------------------------------------------------------------");
         correctAnswers++;
         Console.ReadLine();
     }
     else
     {
-        Console.WriteLine("Wrong! Better luck next time. Please press enter to continue...");
+        Console.WriteLine("-------------------------------------------------------------------------------------------------------");
+        Console.WriteLine("~~~                  Wrong! Better luck next time. Please press enter to continue...                ~~~");
+        Console.WriteLine("-------------------------------------------------------------------------------------------------------");
         Console.ReadLine();
     }
 }
@@ -178,8 +200,11 @@ void GameLoop()
         AskQuestion(difficulty);
     }
     gameHistory.Add(gameHistoryKeyCounter, new List<string>(answersLog));
-    Console.WriteLine($"Congratulations! You've answered {correctAnswers} out of {questionMaxCount} questions!");
-    Console.WriteLine($"To try again, type '1' || to quit to the menu type '2'.");
+    Console.WriteLine("-------------------------------------------------------------------------------------------------------");
+    Console.WriteLine($"~~~     Congratulations! You've answered {correctAnswers} out of {questionMaxCount} questions!     ~~~");
+    Console.WriteLine();
+    Console.WriteLine($"~~~           To try again, type '1' || to quit to the menu type '2'.                              ~~~");
+    Console.WriteLine("-------------------------------------------------------------------------------------------------------");
     int playerResponse = CheckAnswer();
     if (playerResponse == 1)
     {
@@ -193,4 +218,23 @@ void GameLoop()
     }
 }
 
+void RainbowText(string text)
+{
+    ConsoleColor[] colors = {
+        ConsoleColor.Red,
+        ConsoleColor.Yellow,
+        ConsoleColor.Green,
+        ConsoleColor.Cyan,
+        ConsoleColor.Blue,
+        ConsoleColor.Magenta
+    };
+    Random rand = new Random();
+    foreach (char c in text)
+    {
+        Console.ForegroundColor = colors[rand.Next(colors.Length)];
+        Console.Write(c);
+    }
+    Console.ResetColor();
+    Console.WriteLine();
+}
 GameMenu();
